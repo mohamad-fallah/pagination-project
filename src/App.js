a6292557c58881bce1a39f6cbacc615cd13d19be
Mohammadreza Fallahfaal <mfallahfaal@gmail.com>
2024-08-28 19:51:21 +0330

    placeholder completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 export default function App() {
+
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(res => res.json())
+      .then(datas => {
+        setTodos(datas)
+        console.log(datas);        
+      })
+  }, [])
+
+
+
   return (
     <div>
-      
+      {
+        !todos ? "loading" : (
+          <table className="table">
+            <thead>
+            <tr>
+              <th>ID</th>
+              <th>UserId</th>
+              <th>Title</th>
+              <th>Status</th>
+            </tr>
+            </thead>
+            <tbody>
+              {todos.map(todo => (
+                <tr>
+                  <td>{todo.id}</td>
+                  <td>{todo.userId}</td>
+                  <td>{todo.title}</td>
+                  <td>
+                    <p
+                      className={todo.completed ? "btn btn-success" : "btn btn-danger"}
+                    >
+                      {todo.completed ? "complested" : "pending"}
+                    </p>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )
+      }
     </div>
   );
 }
